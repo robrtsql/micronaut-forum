6a15b7f9c4c19979bfb5399c09d74a72bab8e336
robrtsql <robrtsql@gmail.com>
2024-10-06 21:39:46 -0500

    Set up JWT authentication

--- src/main/java/com/robsigler/ForumPostController.java
+++ src/main/java/com/robsigler/ForumPostController.java
@@ -4,11 +4,14 @@ import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Post;
+import io.micronaut.security.annotation.Secured;
+import io.micronaut.security.rules.SecurityRule;
 import jakarta.inject.Inject;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-@Controller
+@Secured(SecurityRule.IS_AUTHENTICATED)
+@Controller("/posts")
 public class ForumPostController {
   @Inject private ForumPostRepository postRepository;
 
